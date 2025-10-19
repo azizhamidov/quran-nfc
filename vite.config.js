95d73a00a79cd581974d3e20e05d32ce1b274331
Abdurahmon Nurov <hamidovaziz657@gmail.com>
2025-10-19 22:53:01 +0500

    Update build and deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/quran-nfc/', // must match your repo name
+  base: '/', // must match your repo name
 })
